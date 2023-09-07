e8929a61b9eebeede74305dd1557e5a3b2cb43f5
Cynthiaogwu <cynthiaogwu3@gmail.com>
2023-09-07 03:38:58 -0700

    qu0te

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <unitstd.h>
+#include <stdio.h>
+
 /**
  * main - A c program that prints a line to the standard e
  *
@@ -7,7 +8,6 @@
  */
 int main(void)
 {
-	write(2,
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
