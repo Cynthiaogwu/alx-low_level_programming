d2ab6690b96450c371c64f2276a6e5894a6dd755
Cynthiaogwu <cynthiaogwu3@gmail.com>
2023-09-08 13:17:27 -0700

    Variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,43 +6,25 @@
  */
 int main(void)
 {
-int num1, num2, num3, num4;
-num1 = '0';
-num2 = '0';
-num3 = '0';
-num4 = '0';
-while (num1 <= '9')
+int i, j;
+for (i = 0; i < 100; i++)
 {
-while (num2 <= '9')
+for (j = 0; j < 100; j++)
 {
-while (num3 <= '9')
+if (i < j)
 {
-while (num4 <= '9')
-{
-if (((num3 + num4) > (num1 + num2)&&num3 >= num1) || num1 < num3)
-{
-putchar(num1);
-putchar(num2);
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(num3);
-putchar(num4);
-putchar(',');
-if (num1 + num2 + num3 + num4 == '8' && num1 == '9')
-{
-break;
-}
-else
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-num3++;
-}
-num2++;
-}
-num1++;
 }
 putchar('\n');
 return (0);
