80767fdedf977d0a45fbdb53630771021307949d
Cynthiaogwu <cynthiaogwu3@gmail.com>
2023-09-20 15:39:51 -0700

    expect the best

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,12 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			str[i] = str[i] - 32;
-		}
+		i++;
 	}
 	return (str);
 }
